8c625e27da436f6294e2e12131f8cfb3dcd082ea
SLOom <illuclem@gmail.com>
2019-04-03 20:59:52 +0200

    Fixing errors

--- src/Illuxatlib.php
+++ src/Illuxatlib.php
@@ -1,85 +1,115 @@
-<?php
-
+<?php 
+/**
+ * This file is part of Illuxat/SLOom.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   Illuxat
+ * @author    SLOom/Clément <admin@illuxat.com>
+ * @copyright 2019 Illuxat
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      https://illuxat.com
+ */
 namespace Illuxat;
 
-class Illuxatlib 
+/**
+ * A PHP library for Illuxat.com
+ *
+ * @category  PHP
+ * @package   Illuxat
+ * @author    SLOom/Clément <admin@illuxat.com>
+ * @copyright 2019 Illuxat
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      https://illuxat.com
+ */
+class Illuxatlib
 {
-   /**
-   * Main domain for Illuxat
-   *
-   * @var   string
-   */
-   private static $illuxat_dom = 'https://api.illuxat.com/';
+    /**
+     * Main domain for Illuxat
+     *
+     * @var string $_illuxat_dom
+     */
+    private static $_illuxat_dom = 'https://api.illuxat.com/';
 
-  /**
-   * Get the informations of a power
-   * 
-   * @param  string
-   * @return array
-   */
-   public static function powerInfo(string $powerName): array 
-   {
-      if (empty($powerName) || strlen($powerName) == 0) {
-         throw new \Exception('You must specify a power');
-      }
+    /**
+     * Get the informations of a power
+     * 
+     * @param string $powerName Name of a xat power
+     * 
+     * @return array
+     */
+    public static function powerInfo(string $powerName): array 
+    {
+        if (empty($powerName) || strlen($powerName) == 0) {
+            throw new \Exception('You must specify a power');
+        }
 
-      $content = self::getContent(self::$illuxat_dom . 'powerApi?power=' . $powerName);
+        $content = self::_getContent(
+            self::$_illuxat_dom . 'powerApi?power=' . $powerName
+        );
 
-      if (empty($content['response'])) {
-         throw new \Exception("Error Processing Request");     
-      }
+        if (empty($content['response'])) {
+            throw new \Exception("Error Processing Request");     
+        }
 
-      $content = json_decode($content['response'], true);
+        $content = json_decode($content['response'], true);
 
-      if (json_last_error() !== JSON_ERROR_NONE) {
-         throw new \Exception('Invalid JSON.');
-      }
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception('Invalid JSON.');
+        }
 
-      return $content;
-   }
+        return $content;
+    }
 
-   /**
-    * Get the informations of the latest power
-    * @return mixed
-    */
-   public static function getLatest(): ?array 
-   {
-      $content = self::getContent(self::$illuxat_dom . 'latestpower');
+    /**
+     * Get the informations of the latest power
+     *
+     * @return mixed
+     */
+    public static function getLatest(): ?array 
+    {
+        $content = self::_getContent(self::$_illuxat_dom . 'latestpower');
 
-      if (empty($content['response'])) {
-         throw new \Exception("Error Processing Request");     
-      }
+        if (empty($content['response'])) {
+            throw new \Exception("Error Processing Request");     
+        }
 
-      $content = json_decode($content['response'], true);
+        $content = json_decode($content['response'], true);
 
-      if (json_last_error() !== JSON_ERROR_NONE) {
-         throw new \Exception('Invalid JSON.');
-      }
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception('Invalid JSON.');
+        }
 
-      return $content;
+        return $content;
 
-   }
+    }
 
-  /**
-   *  Get content of a URL
-   * 
-   * @param  string
-   * @return mixed
-   */
-   private function getContent(string $url): ?array
-   {
-      if (empty($url) || strlen($url) == 0) {
-         throw new \Exception('You must specify a url');
-      }
+    /**
+     *  Get content of a URL
+     * 
+     * @param string $url Url to fetch content
+     * 
+     * @return mixed
+     */
+    private function _getContent(string $url): ?array
+    {
+        if (empty($url) || strlen($url) == 0) {
+            throw new \Exception('You must specify a url');
+        }
 
-      $curlInit = curl_init();
-      curl_setopt($curlInit, CURLOPT_URL, $url); 
-      curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, 1); 
-      $output = curl_exec($curlInit); 
-      curl_close($curlInit);
+        $curlInit = curl_init();
+        curl_setopt($curlInit, CURLOPT_URL, $url); 
+        curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, 1); 
+        $output = curl_exec($curlInit); 
+        curl_close($curlInit);
 
-      return [
+        return [
          'response' => $output
-      ];
-   }
+        ];
+    }
 }
